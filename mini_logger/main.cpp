b37357d77af30691926c5bc2029ef9b10764bc37
futinn@gmail.com <futinn@gmail.com>
2018-12-17 17:46:18 +0100

    init vs prj

--- mini_logger/main.cpp
+++ mini_logger/main.cpp
@@ -1,9 +1,9 @@
-#include<mini_logger.h>
+#include"mini_logger.h"
 
 int main()
 {
   Logger logger("log");
-  logger<<"just a example logging file"<<logger.endl；
+  logger << "just a example logging file" << logger.endl;
   logger<<logger.endl<<"testing endl";
   
   return 0;
